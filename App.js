8d8aabfea3c45b0561290bca00e82518688d0d0d
juniper3lee12 <107044751+juniper3lee12@users.noreply.github.com>
2025-03-07 23:53:52 +1000

    Update App.js

--- App.js
+++ App.js
@@ -11,70 +11,55 @@ import Update from "./screens/Update";
 import Home from "./screens/Home";
 import { ThemeProvider } from "./context/theme";
 import * as eva from "@eva-design/eva";
-import { EvaIconsPack } from "@ui-kitten/eva-icons";
-import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
-import { ThemeContext } from "../Front-end/context/theme-context";
+import { ApplicationProvider } from "@ui-kitten/components";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-  // background color theme
-  const [theme, setTheme] = React.useState("light");
-
-  const toggleTheme = () => {
-    const nextTheme = theme === "light" ? "dark" : "light";
-    setTheme(nextTheme);
-  };
-
   return (
-    <>
-      <IconRegistry icons={EvaIconsPack} />
-      <ThemeContext.Provider value={{ theme, toggleTheme }}>
-        <ApplicationProvider {...eva} theme={eva[theme]}>
-          <NavigationContainer>
-            <ThemeProvider>
-              {/* Always visit HomeScreen first */}
-              <Drawer.Navigator initialRouteName="Home">
-                <Drawer.Screen
-                  name="Home"
-                  component={Home}
-                  options={{ drawerLabel: "Home" }}
-                />
-                <Drawer.Screen
-                  name="Meditate"
-                  component={Meditate}
-                  options={{ drawerLabel: "Meditate" }}
-                />
-                <Drawer.Screen
-                  name="My Space"
-                  component={MySpace}
-                  options={{ drawerLabel: "MySpace" }}
-                />
-                <Drawer.Screen
-                  name="Update"
-                  component={Update}
-                  options={{ drawerLabel: "Update" }}
-                />
-                <Drawer.Screen
-                  name="Login"
-                  component={Login}
-                  options={{ drawerLabel: "Login" }}
-                />
-                <Drawer.Screen
-                  name="About"
-                  component={About}
-                  options={{ drawerLabel: "About" }}
-                />
-                <Drawer.Screen
-                  name="Setting"
-                  component={Setting}
-                  options={{ drawerLabel: "Setting" }}
-                />
-              </Drawer.Navigator>
-            </ThemeProvider>
-          </NavigationContainer>
-        </ApplicationProvider>
-      </ThemeContext.Provider>
-    </>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <ThemeProvider>
+          {/* Always visit HomeScreen first */}
+          <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen
+              name="Home"
+              component={Home}
+              options={{ drawerLabel: "Home" }}
+            />
+            <Drawer.Screen
+              name="Meditate"
+              component={Meditate}
+              options={{ drawerLabel: "Meditate" }}
+            />
+            <Drawer.Screen
+              name="My Space"
+              component={MySpace}
+              options={{ drawerLabel: "MySpace" }}
+            />
+            <Drawer.Screen
+              name="Update"
+              component={Update}
+              options={{ drawerLabel: "Update" }}
+            />
+            <Drawer.Screen
+              name="Login"
+              component={Login}
+              options={{ drawerLabel: "Login" }}
+            />
+            <Drawer.Screen
+              name="About"
+              component={About}
+              options={{ drawerLabel: "About" }}
+            />
+            <Drawer.Screen
+              name="Setting"
+              component={Setting}
+              options={{ drawerLabel: "Setting" }}
+            />
+          </Drawer.Navigator>
+        </ThemeProvider>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
